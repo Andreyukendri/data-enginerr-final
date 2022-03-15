c8a4a260c9ab03072c68c64bfb96cb3943820c24
Andreyukendri <95620394+Andreyukendri@users.noreply.github.com>
2022-03-15 20:57:23 +0700

    Add files via upload

---
+++ final_andre_spark.py
@@ -0,0 +1,42 @@
+#transformation.py
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("My PySpark code") \
+    .getOrCreate()
+
+
+#convert and write csv to parquet
+df_temp = spark.read.options(header='true', inferSchema='true').csv("gs://file_andre/Input/GlobalLandTemperaturesByCity.csv")
+df_temp.write.mode("Overwrite").parquet("gs://file_andre/raw/globaltempbycity.parquet")
+
+df_airport = spark.read.options(header='true', inferSchema='true').csv("gs://file_andre/Input/airport-codes_csv.csv")
+df_airport.write.mode("Overwrite").parquet("gs://file_andre/raw/airportcodes.parquet")
+
+df_immigration = spark.read.options(header='true', inferSchema='true').csv("gs://file_andre/Input/immigration_data_sample.csv")
+df_immigration.write.mode("Overwrite").parquet("gs://file_andre/raw/immigration.parquet")
+
+df_port = spark.read.options(header='true', inferSchema='true').csv("gs://file_andre/Input/USPORT.csv")
+df_port.write.mode("Overwrite").parquet("gs://file_andre/raw/usport.parquet")
+
+df_state = spark.read.options(header='true', inferSchema='true').csv("gs://file_andre/Input/USSTATE.csv")
+df_state.write.mode("Overwrite").parquet("gs://file_andre/raw/usstate.parquet")
+
+df_country = spark.read.options(header='true', inferSchema='true').csv("gs://file_andre/Input/USCOUNTRY.csv")
+df_country.write.mode("Overwrite").parquet("gs://file_andre/raw/uscountry.parquet")
+
+# The delimiter for this file (us-cities-demographics.csv) is semicolon (;)
+# We have to change the default delimiter
+df_demographic = spark.read.options(header="true", inferSchema="true", delimiter=";").csv("gs://file_andre/Input/us-cities-demographics.csv")
+df_demographic.printSchema()
+# There is a problem in Header of this df, 
+# Therefore, we need to rename the header before write it into parquet file
+# Header: City;State;Median Age;Male Population;Female Population;Total Population;Number of Veterans;Foreign-born;Average Household Size;State Code;Race;Count
+
+# Rename all column names with new column names
+demographic_new_col_names = ["City", "State", "MedianAge", "MalePopulation", "FemalePopulation", "TotalPopulation", "NumberOfVeterans", "ForeignBorn", "AverageHouseholdSize", "StateCode", "Race", "Count"]
+df_demographic = df_demographic.toDF(*demographic_new_col_names)
+
+# Then, we convert it into parquet
+df_demographic.write.mode("Overwrite").parquet("gs://file_andre/raw/demographic.parquet")
